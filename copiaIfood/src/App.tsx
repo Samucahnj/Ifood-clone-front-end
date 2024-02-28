64844c65f281427e39c437cd07fd651ce45dc099
Samuel <samuel.hnj@gmail.com>
2024-02-27 22:28:58 -0300

    first commit

---
+++ copiaIfood/src/App.tsx
@@ -0,0 +1,10 @@
+import logo from "./images/ifood-logo-0.png"
+
+export function App() {
+  return <>
+  <img src={logo} width={100}/>
+  <h1 className="text-3xl font-bold underline">  Hello world!</h1>
+
+</>
+} 
+
